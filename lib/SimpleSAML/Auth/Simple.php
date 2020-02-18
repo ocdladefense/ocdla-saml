09293c92f47b29089aaf5ca84c5f0764681d84fe
Jose Bernal <jbernal.web.dev@gmail.com>
2020-02-17 19:53:31 -0800

    Add better session handling.

--- lib/SimpleSAML/Auth/Simple.php
+++ lib/SimpleSAML/Auth/Simple.php
@@ -85,20 +85,11 @@ class SimpleSAML_Auth_Simple {
 	/**
 	 * Require the user to be authenticated.
 	 *
-	 * If the user is authenticated, this function returns immediately.
-	 *
-	 * If the user isn't authenticated, this function will authenticate the
-	 * user with the authentication source, and then return the user to the
-	 * current page.
-	 *
-	 * This function accepts an array $params, which controls some parts of
-	 * the authentication. See the login()-function for a description.
+	 * Copy of ->requireAuth, above; modified to take a loaded $session object.
 	 *
 	 * @param array $params  Various options to the authentication request.
 	 */
 	public function requireAuth2($session,$params=array()) {
-
-		// $session = SimpleSAML_Session::getSession($sessionId);
 
 		if ($session->isValid($this->authSource)) {
 			/* Already authenticated. */
